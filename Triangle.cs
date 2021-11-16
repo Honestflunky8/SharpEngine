60f6c2f564182ebcac511b2318044be42dc8b883
Honestflunky8 <90176781+Honestflunky8@users.noreply.github.com>
2021-11-16 09:53:56 +0100

    Bounds working and Transform file created

--- Triangle.cs
+++ Triangle.cs
@@ -10,7 +10,7 @@ namespace SharpEngine {
 		uint vertexArray;
 		uint vertexBuffer;
 
-		public float CurrentScale { get; private set; }
+		public Transform Transform { get; }
 
 		public Material material;
             
@@ -18,7 +18,7 @@ namespace SharpEngine {
 			this.vertices = vertices;
 			this.material = material;
 			LoadTriangleIntoBuffer();
-			this.CurrentScale = 1f;
+			this.Transform = new Transform();
 		}
 		
 		 void LoadTriangleIntoBuffer() {
@@ -34,17 +34,17 @@ namespace SharpEngine {
 		}
 
 		public Vector GetMinBounds() {
-			var min = this.vertices[0].position;
+			var min = this.Transform.Matrix * this.vertices[0].position;
 			for (var i = 1; i < this.vertices.Length; i++) {
-				min = Vector.Min(min, this.vertices[i].position);
+				min = Vector.Min(min,this.Transform.Matrix* this.vertices[i].position);
 			}
 			return min;
 		}
             
 		public Vector GetMaxBounds() {
-			var max = this.vertices[0].position;
+			var max = this.Transform.Matrix * this.vertices[0].position;
 			for (var i = 1; i < this.vertices.Length; i++) {
-				max = Vector.Max(max, this.vertices[i].position);
+				max = Vector.Max(max, this.Transform.Matrix * this.vertices[i].position);
 			}
 
 			return max;
@@ -54,17 +54,9 @@ namespace SharpEngine {
 			return (GetMinBounds() + GetMaxBounds()) / 2;
 		}
 
-		public void Scale(float multiplier) {
-
-		}
-
-		public void Move(Vector direction) {
-			this.transform *= Matrix.Translation(direction);
-		}
-
 		public unsafe void Render() {
 			this.material.Use();
-			this.material.SetTransform(this.transform);
+			this.material.SetTransform(this.Transform.Matrix);
 			glBindVertexArray(vertexArray);
 			glBindBuffer(GL_ARRAY_BUFFER, this.vertexBuffer);
 			fixed (Vertex* vertex = &this.vertices[0]) {
@@ -73,9 +65,6 @@ namespace SharpEngine {
 			glDrawArrays(GL_TRIANGLES, 0, this.vertices.Length);
 			glBindVertexArray(0);
 		}
-
-		public void Rotate(float rotation) {
-			
-		}
+		
 	}
 }
